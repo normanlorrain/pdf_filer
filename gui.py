e4e82811ee47f98b8548a98b59bb45c802e18635
Norman (Admin) Lorrain <normanlorrain@gmail.com>
2023-10-26 09:09:01 -0600

    These are modules from an old "pdfrotate" project. Baseline.

---
+++ gui.py
@@ -0,0 +1,118 @@
+import flet as ft
+import pdf
+import sys
+
+pdfFile = None
+
+img = None
+
+
+# We need to catch the window close event, so that we can clean up.  It's not automatic, sadly.
+def page_window(e):
+    global pdfFile
+
+    print(e.data)
+    if e.data == "close":
+        if pdfFile:
+            del pdfFile
+        e.page.window_destroy()
+
+
+def main(page: ft.Page):
+    page.window_center()
+    page.on_window_event = page_window
+    page.window_prevent_close = True
+
+    def pick_files_result(e: ft.FilePickerResultEvent):
+        if not e.files:
+            print("Cancelled!")
+            return
+        fileDetails = e.files[0]
+
+        print(f"User opens {fileDetails.name}")
+        openFile(fileDetails.path)
+
+    def openFile(path):
+        global pdfFile
+        global img
+        pdfFile = pdf.pdf(path)
+        if img:
+            page.controls.pop()
+        img = pageImage(0)
+        page.expand = True
+        page.add(img)
+        btnSave.disabled = True
+        bntDn.disabled = False
+        bntUp.disabled = False
+        btn180.disabled = False
+        page.update()
+        pass
+
+    def pageImage(pageNumber):
+        return ft.Image(
+            src_base64=pdfFile.get_page(pageNumber),
+            width=pdf.width,
+            height=pdf.height,
+            # fit=ft.ImageFit.FIT_HEIGHT,
+            expand=True,
+        )
+
+    def changePage(down=False):
+        global img
+        pageNumber = pdfFile.changePage(down)
+        page.controls.pop()
+        img = pageImage(pageNumber)
+        page.add(img)
+
+        page.update()
+
+    def rotate(foo):
+        page.controls.pop()
+        pdfFile.rotate()
+        img = pageImage(pdfFile.currentPage)
+        page.add(img)
+        btnSave.disabled = False
+        page.update()
+
+    def save(foo):
+        print("Saving file")
+        pdfFile.save()
+        btnSave.disabled = True
+        page.update()
+
+    pick_files_dialog = ft.FilePicker(on_result=pick_files_result)
+    page.overlay.append(pick_files_dialog)
+
+    page.title = "PDF Rotate"
+    page.window_width = 85 * 5  # pdf.width
+    page.window_height = 110 * 5  # pdf.height
+    # page.window_resizable = False  # window is not resizable
+
+    btnOpen = ft.TextButton(
+        text="Open",
+        on_click=lambda _: pick_files_dialog.pick_files(
+            dialog_title="Open PDf file",
+            file_type=ft.FilePickerFileType.ANY,
+            allowed_extensions=["pdf"],
+            allow_multiple=False,
+        ),
+    )
+
+    btnSave = ft.TextButton("Save", on_click=save, disabled=True)
+
+    bntUp = ft.TextButton(
+        "PgUp", on_click=lambda _: changePage(down=False), disabled=True
+    )
+    bntDn = ft.TextButton(
+        "PgDn", on_click=lambda _: changePage(down=True), disabled=True
+    )
+    btn180 = ft.TextButton("180", on_click=rotate, disabled=True)
+
+    page.add(ft.Row(spacing=0, controls=[btnOpen, btnSave, bntUp, bntDn, btn180]))
+
+    if len(sys.argv) > 1:
+        openFile(sys.argv[1])
+
+
+if __name__ == "__main__":
+    ft.app(target=main)
---
+++ pdf.py
@@ -0,0 +1,64 @@
+import sys
+import base64
+import fitz
+import pathlib
+import os
+
+print(fitz.__doc__)
+
+width, height = fitz.paper_size("letter")
+
+
+class pdf:
+    def __init__(self, fname):
+
+        self.name = fname
+        inputFile = open(fname, "rb")
+        bytes = inputFile.read()
+        inputFile.close()
+
+        self.doc = fitz.Document(stream=bytes)
+
+        self.page_count = len(self.doc)
+        self.currentPage = 0
+
+        title = "PyMuPDF display of '%s', pages: %i" % (fname, self.page_count)
+
+    def __del__(self):
+        pass
+
+    # read the page data
+    def get_page(self, pno, zoom=False, max_size=None):
+        global width, height
+
+        pixmap = self.doc.get_page_pixmap(
+            pno,
+        )  # *, matrix: matrix_like = Identity, dpi=None, colorspace: Colorspace = csRGB, clip: rect_like = None, alpha: bool = False, annots: bool = True)
+        width = pixmap.width
+        height = pixmap.height
+        print(f"get page {pno}: {width} x {height}")
+        bytes = pixmap.tobytes()
+        enc = base64.b64encode(bytes)
+        strEnc = enc.decode("ascii")
+        return strEnc
+
+    def changePage(self, down=True):
+        if down:
+            self.currentPage += 1
+        else:
+            self.currentPage -= 1
+        if self.currentPage < 0:
+            self.currentPage = 0
+        if self.currentPage >= self.doc.page_count:
+            self.currentPage = self.doc.page_count - 1
+
+        return self.currentPage
+
+    def rotate(self):
+        current_rotation = self.doc[self.currentPage].rotation
+        new_rotation = (current_rotation + 180) % 360
+        self.doc[self.currentPage].set_rotation(new_rotation)
+
+    def save(self):
+        self.backup.keep = True
+        self.doc.save(self.name)
