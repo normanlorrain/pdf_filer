b05cb09de63ee44548d96989a467fff7cdbdd8ca
Norman Lorrain <normanlorrain@gmail.com>
2023-11-02 14:38:26 -0600

    small experiment with threading.  keeping in comments.

--- gui/layout.py
+++ gui/layout.py
@@ -1,7 +1,6 @@
 import flet as ft
 
-pdfFile = None
-
+_page = None
 # References to the fields so we can update them
 imgPDF = ft.Ref[ft.Image]()
 colDestination = ft.Ref[ft.Column]()  # The right-hand column, containing these:
@@ -14,18 +13,19 @@ txtDstFileName = ft.Ref[ft.Text]()
 
 # We need to catch the window close event, so that we can clean up.  It's not automatic, sadly.
 def windowEvent(e):
-    global pdfFile
-
     print(e.data)
     if e.data == "close":
-        if pdfFile:
-            del pdfFile
+        # if pdfFile:
+        #     del pdfFile
         e.page.window_destroy()
 
 
 def mainWindow(
     page: ft.Page, onNextBtn=None, onPgUp=None, onPgDown=None, onMoveBtn=None
 ):
+    global _page
+    _page = page
+
     page.window_center()
     page.on_window_event = windowEvent
     page.window_prevent_close = True
@@ -81,7 +81,7 @@ def mainWindow(
 
     page.pubsub.subscribe(on_message)  # type: ignore
     print("sending")
-    page.pubsub.send_all("Test Messsage")
+    page.pubsub.send_all("Test Messsage")  # type: ignore
     print("Sent")
 
 
@@ -92,4 +92,5 @@ if __name__ == "__main__":
     # thread = threading.Thread(target=ft.app, kwargs={"target": mainWindow})
     # thread.start()
     # print("thread started")
+    # _page.pubsub.send_all("Sending from outside thread")
     # thread.join()
