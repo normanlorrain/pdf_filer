6078382e94524b1f9bd42381a6a0071b7c691998
Norman Lorrain <normanlorrain@gmail.com>
2023-11-07 08:44:47 -0700

    Moved "other" type out of config file.

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import tomllib
 
-with open("config.toml","rb") as fp:
+with open("config.toml", "rb") as fp:
     config = tomllib.load(fp)
+
+# Used to be in the config file.  Hard-code it.
+config["TYPES"]["other"] = "{other} faxed {date}"
 
 pass
